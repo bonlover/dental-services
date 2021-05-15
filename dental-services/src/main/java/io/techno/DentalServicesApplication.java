cb2a8cb7254bfc544765ddc7109dcd03ecc2e1fa
bonlover <yungyalsen@gmail.com>
2021-05-15 16:19:57 -0400

    first commit

---
+++ dental-services/src/main/java/io/techno/DentalServicesApplication.java
@@ -0,0 +1,13 @@
+package io.techno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DentalServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DentalServicesApplication.class, args);
+	}
+
+}
