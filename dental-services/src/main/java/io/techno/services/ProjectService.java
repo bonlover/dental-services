538abc01ae3943db5f3870bd962d02e66dabd070
bonlover <yungyalsen@gmail.com>
2021-05-16 02:14:03 -0400

    project postRequest exception handling exception and service updated

--- dental-services/src/main/java/io/techno/services/ProjectService.java
+++ dental-services/src/main/java/io/techno/services/ProjectService.java
@@ -1,6 +1,7 @@
 package io.techno.services;
 
 import io.techno.domain.Project;
+import io.techno.exceptions.ProjectIdException;
 import io.techno.repositories.ProjectRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,9 +16,14 @@ public class ProjectService {
 	
 	public Project saveOrUpdateProject(Project project) {
 		
-		//logic
+		try {
+			project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+			return projectRepository.save(project);
+			
+		} catch(Exception e) {
+			throw new ProjectIdException("Project ID '" +project.getProjectIdentifier().toUpperCase()+"' already exist");
+		}
 		
-		return projectRepository.save(project);
 	}
 
 }
